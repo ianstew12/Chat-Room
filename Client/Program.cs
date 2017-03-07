3ef6716d6f7285ccd8a2b2c576a163b7f6604173
ianstew12 <ian.stew12@gmail.com>
2017-03-07 17:28:43 -0600

    server-client connection established

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
+using System.Net;
 namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            {
+                TcpClient serverTPC = new TcpClient();
+                IPAddress localAddr = IPAddress.Parse("192.168.0.137");
+                serverTPC.Connect(localAddr, 2017);
+                //TcpListener server = null;
+                Console.Read();
+            }
         }
     }
 }
